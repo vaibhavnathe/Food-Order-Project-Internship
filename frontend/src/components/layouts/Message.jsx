1a62853a23296b9d335aebeddd34261ba3cb81e4
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-07 00:26:24 +0530

    Feat: Add Message Component which is used for showing Success and Error messages

---
+++ frontend/src/components/layouts/Message.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Message({variant, children}) {
+
+
+
+  return (
+    <div className={`alert alert-${variant}`}>
+      {children}
+    </div>
+  )
+}
