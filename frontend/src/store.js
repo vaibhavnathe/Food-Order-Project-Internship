ec8547081516f96c749657e74ad1a6c9d0606103
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-07 01:09:13 +0530

    Feat: Add store.js file which is redux store contains the all data which manage by the states

---
+++ frontend/src/store.js
@@ -0,0 +1,31 @@
+
+import {legacy_createStore as createStore, 
+    combineReducers, 
+    applyMiddleware, 
+    compose 
+} from 'redux';
+
+import thunk from 'redux-thunk';        // helps to run the data fetching (delayed work)
+import { restaurantReducer } from './reducer/restaurantReducer';
+import { menuReducer } from './reducer/menuReducer';
+
+// create store -> just like array or object
+// combineReducers -> to group the reducer
+// applyMiddleware -> it is used to use any middleware
+// compose -> to read the function from right to left
+
+const reducer = combineReducers({
+    restaurants : restaurantReducer,
+    menus : menuReducer,
+});
+
+// helps us to connect the reactDevTools 
+const composeenhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = [thunk];
+
+const store = createStore(reducer, 
+    composeenhancers(applyMiddleware(...middleware))
+);
+
+export default store;
