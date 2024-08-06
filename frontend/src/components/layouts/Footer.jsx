9ac31bb5e8611d730dac9329021fe60f0a22af3e
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-07 00:24:28 +0530

    Feat: Add Footer Component which represnts the footer of the website

---
+++ frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p className="text-center">
+            Food Delivery Website - 2024-25. All Rights Reserved By WSA...
+        </p>
+      </footer>
+    </>
+  )
+}
