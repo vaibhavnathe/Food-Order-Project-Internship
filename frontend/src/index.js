04f1e5c2aeeaa42107a623500b132137c15dd6e3
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-10 23:54:39 +0530

    Feat: React-alert Added in Index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from "./store";
+import { transitions, positions, Provider as AlertProvider } from "react-alert"
+import AlertTemplate from "react-alert-template-basic"
+
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </AlertProvider>
   </Provider>
-  
+
 );
 
 // If you want to start measuring performance in your app, pass a function
