b05def3da16ad54dc3e5332f53985f5f0491600d
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-10 23:55:48 +0530

    Feat: User Action Created

---
+++ frontend/src/action/userAction.js
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { CLEAR_ERRORS, LOAD_USER_FAIL, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAIL, LOGOUT_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS } from "../constants/userConstant"
+
+export const userLogin = (email, password) = async(dispatch) => {
+
+    try{
+        dispatch({type:LOGIN_REQUEST});
+        const config = {
+            headers:{
+                "Content-Type" : "application/json",
+            } 
+        };
+
+        const {data} = await axios.post(
+            "/api/v1/users/login", 
+            {email, password},
+            config
+        );
+
+        dispatch({
+            type:LOGIN_SUCCESS, 
+            payload: data.data.user,
+        });
+
+
+    }catch(error){
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: "Login Failed"
+        })
+    }
+}
+
+
+// Register
+export const register = (userData)=> async(dispatch) => {
+    
+    try{
+
+        dispatch({type: REGISTER_USER_REQUEST});
+        const config = {
+            headers:{"Content-Type" : "multipart/fprm-data"},
+        };
+
+        const {data} = await axios.post('/api/v1/users/signup', userData, config);
+
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.data.user,
+        });
+
+        return data.data.user; // useful if the calling function needs this data
+
+    }catch(error){
+        dispatch({type:REGISTER_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+// Load User Action
+
+export const loadUser = () => async(dispatch) =>{
+    try{
+
+        dispatch({type: LOAD_USER_REQUEST});
+
+        const {data} = await axios.get('/api/v1/users/me')
+        dispatch({
+            type: LOAD_USER_SUCCESS,
+            payload:data.user
+        });
+
+    }catch(error){
+        dispatch({
+            type:LOAD_USER_FAIL,
+            payload:error.response.data.message
+        })
+    }
+}
+
+
+// Logout Action
+export const logout = () => async(dispatch) => {
+    try{
+        await axios.get('/api/v1/users/logout');
+        dispatch({
+            type: LOGOUT_SUCCESS,
+        });
+
+    }catch(error){
+        dispatch({
+            type:LOGOUT_FAIL,
+            payload: error.response.data.message,
+        });
+    }
+};
+
+
+// Clear Errors
+export const clearErrors = () => async(dispatch) =>{
+
+    dispatch({
+        type: CLEAR_ERRORS,
+    });
+};
