f3a5622a41540faf958365eae831ebc59d6d728f
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-26 22:01:13 +0530

    Feat: Made Header Component dynamic by adding the functinality of user register and login

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -1,62 +1,106 @@
 import React from 'react';
 import Search from './Search';
 import { Link } from 'react-router-dom';
+import { useAlert } from 'react-alert';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../action/userAction';
 
 const Header = () => {
 
-  return (
-    <nav className="navbar row sticky-top">
+    const alert = useAlert();
+    const dispatch = useDispatch();
 
-        {/* Logo */}
-        <div className="col-12 col-md-3">
-            <Link to='/'>
-                <img src="./images/logo.webp" alt="LogoImage" className="logo" />
-            </Link>
-            
-        </div>
+    const { user, loading } = useSelector((state) => state.auth);
 
-        {/* Search Bar and Search Icon*/}
-        <div className="col-12 col-md-6 mt-2 mt-md-6">
-            <Search/>
-        </div>
+    const logoutHandler = () => {
+        dispatch(logout());
+        alert.success("Logged Out Successfully!");
+    }
 
-        {/* Cart with cart no. */}
-        <div className="col-12 col-md-3 mt-4 mt-md-0">
-            <span className="ml-3" id="cart">
-                Cart
-            </span>
-            <span className="ml-1" id='cart_count'>
-                0
-            </span>
+    return (
+        <nav className="navbar row sticky-top">
 
-            {
-                10 < 5 ? (
-                    <>
-                        <div className="ml-4 dropdown d-inline">
-                            <figure className="avatar avatar-nav">
-                                <img src="/images/images.png" alt="avatar" 
-                                className="rounded-circle" />
-                            </figure>
-                            <span style={{color:"white", fontWeight:"bolder"}}>
-                                WSA Developer
-                            </span>
-                        </div>
-                    </>
-                ) 
-                : (
-                    <>
-                        <div className="btn ml-4" id="login_btn">
-                            Login
-                        </div>
-                    </>
-                )
-            }
+            {/* Logo */}
+            <div className="col-12 col-md-3">
+                <Link to='/'>
+                    <img src="/images/logo.webp" alt="LogoImage" className="logo" />
+                </Link>
 
-        </div>
-    
+            </div>
 
-    </nav>
-  );
+            {/* Search Bar and Search Icon*/}
+            <div className="col-12 col-md-6 mt-2 mt-md-6">
+                <Search />
+            </div>
+
+            {/* Cart with cart no. */}
+            <div className="col-12 col-md-3 mt-4 mt-md-0">
+                <span className="ml-3" id="cart">
+                    Cart
+                </span>
+                <span className="ml-1" id='cart_count'>
+                    0
+                </span>
+
+                {
+                    user ? (
+                        <>
+                            <div className="ml-4 dropdown d-inline">
+                                <Link to='/' className='btn dropdown-toggle text-white mt-4'
+                                    type='button'
+                                    id='dropDownMenuButton'
+                                    data-toggle='dropdown'
+                                    aria-haspopup='true'
+                                    aria-expanded='false'
+                                >
+                                    <figure className="avatar avatar-nav">
+                                        <img src="/images/images.png" alt="avatar"
+                                            className="rounded-circle" />
+                                    </figure>
+                                    <span >
+                                        {user && user.name}
+                                    </span>
+                                </Link>
+
+                                <div className="dropdown-menu" 
+                                    aria-labelledby='dropDownMenuButton'
+                                >
+                                    <Link className='dropdown-item' 
+                                        to='/eats/orders/me/myOrders'>
+                                            Orders
+                                    </Link>
+
+                                    <Link className='dropdown-item' 
+                                        to='/users/me'>
+                                            Profile
+                                    </Link>
+
+                                    <Link className='dropdown-item' 
+                                            to='/'
+                                            onClick={logoutHandler}
+                                    >
+                                            Logout
+                                    </Link>
+                                </div>
+
+                            </div>
+                        </>
+                    )
+                        : (
+                            !loading && (
+                            
+                                <Link to='/users/login' className="btn ml-4" id="login_btn">
+                                    Login
+                                </Link>
+                            )
+                        )
+                }
+
+            </div>
+
+
+        </nav>
+    );
 };
 
 export default Header;
