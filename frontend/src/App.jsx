f15528cc21ad82c80713c17e2fb8120399af3aad
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-26 21:58:28 +0530

    Feat: made Login Component Dynamic in which added functionality of user login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/layouts/Header";
 import Home from "./components/layouts/Home";
@@ -7,8 +7,19 @@ import Menu from "./components/layouts/Menu";
 import Cart from "./components/cart/Cart";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Register from '../src/components/users/Register'
+import Login from "./components/users/Login";
+import store from './store'
+import { loadUser } from "./action/userAction";
 
 export default function App() {
+
+  // dipatched exactly once when the component is first rendered , and check if user is authenticated or not
+  console.log(store);
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  },[])
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -21,6 +32,7 @@ export default function App() {
             <Route path="/" element={<Home />}/>
             <Route path="/eats/stores/:id/menus" element={<Menu/>}/>
             <Route path="/users/signup" element={<Register/>} />
+            <Route path="/users/login" element={<Login/>} />
 
           </Routes>
         </div>
