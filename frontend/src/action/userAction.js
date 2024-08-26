41c9b422f0a7b967f3ca240b92c60953d784ab46
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-26 22:02:30 +0530

    Feat: modified changes is userAction file

--- frontend/src/action/userAction.js
+++ frontend/src/action/userAction.js
@@ -1,7 +1,19 @@
 import axios from "axios";
-import { CLEAR_ERRORS, LOAD_USER_FAIL, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAIL, LOGOUT_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS } from "../constants/userConstant"
+import { 
+    CLEAR_ERRORS, 
+    LOAD_USER_FAIL, 
+    LOAD_USER_REQUEST, 
+    LOAD_USER_SUCCESS, 
+    LOGIN_FAIL, 
+    LOGIN_REQUEST, 
+    LOGIN_SUCCESS, 
+    LOGOUT_FAIL, 
+    LOGOUT_SUCCESS, 
+    REGISTER_USER_FAIL, 
+    REGISTER_USER_REQUEST, 
+    REGISTER_USER_SUCCESS } from "../constants/userConstant"
 
-export const userLogin = (email, password) = async(dispatch) => {
+export const login = (email, password) => async(dispatch) => {
 
     try{
         dispatch({type:LOGIN_REQUEST});
