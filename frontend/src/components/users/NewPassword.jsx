150433f29ba14516a25981e38ca7c39a0fbb0a0c
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-27 13:07:57 +0530

    Feat: Made the NewPassword Component dynamic by adding the functionality of reset password

--- frontend/src/components/users/NewPassword.jsx
+++ frontend/src/components/users/NewPassword.jsx
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { clearErrors, resetPassword } from "../../action/userAction";
 
 const NewPassword = () => {
+
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const {error, success} = useSelector((state) => state.forgotPassword);
+  const {token} = useParams();
+
+  useEffect(() => {
+    if(error){
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    if(success){
+      alert.success("Password Updated Successfully!");
+      navigate("/users/login");
+    }
+  },[dispatch, alert, error,success, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.set("password", password);
+    formData.set("passwordConfirm", passwordConfirm);
+
+    dispatch(resetPassword(token, formData));
+  }
+
   return (
     <>
       <div className="row wrapper">
         <div className="col-10 col-lg-5">
-          <form className="shadow-lg">
+          <form className="shadow-lg" onSubmit={submitHandler}>
+
             <h1 className="mb-3">New Password</h1>
+
             <div className="form-group">
               <label htmlFor="password_field">Password</label>
               <input
                 type="password"
                 id="password_field"
                 className="form-control"
-                value=""
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
@@ -23,7 +62,8 @@ const NewPassword = () => {
                 type="password"
                 id="confirm_password_field"
                 className="form-control"
-                value=""
+                value={passwordConfirm}
+                onChange={(e) => setPasswordConfirm(e.target.value)}
               />
             </div>
 
@@ -34,6 +74,7 @@ const NewPassword = () => {
             >
               Set Password
             </button>
+
           </form>
         </div>
       </div>
