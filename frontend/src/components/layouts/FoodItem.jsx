a9a37e2996b0074cb878cb0d85c60d39b2fce0bc
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-08-28 23:22:31 +0530

    Feat: Made the FoodItem dynamic by adding the functionality of AddToCart

--- frontend/src/components/layouts/FoodItem.jsx
+++ frontend/src/components/layouts/FoodItem.jsx
@@ -1,10 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAlert } from 'react-alert';
 import { LiaRupeeSignSolid } from "react-icons/lia";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addItemToCart, removeItemFromCart, updateCartQuantity } from '../../action/cartAction';
 
-export default function FoodItem({fooditem}) {
+export default function FoodItem({fooditem, restaurant}) {
 
-    const alert = useAlert();;
+    const [quantity, setQuantity] = useState(1);
+    const [showButtons, setShowButtons] = useState(false);
+
+    const {isAuthenticated, user} = useSelector((state) => state.auth);
+    const cartItems = useSelector((state) => state.cart.cartItems);
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const alert = useAlert();
+
+    useEffect(() => {
+        const cartItem = cartItems.find(
+            (items) => items.foodItem._id === fooditem._id
+        );
+        if(cartItem){
+            setQuantity(cartItem.quantity);
+            setShowButtons(true);
+        }
+        else{
+            setQuantity(1);
+            setShowButtons(false);
+        }
+
+    },[cartItems, fooditem])
+
+    const increaseQty = () => {
+        if(quantity < fooditem.stock){
+            const newQuantity = quantity + 1;
+            setQuantity(newQuantity);
+            dispatch(updateCartQuantity(fooditem._id, newQuantity, alert));
+        }
+        else{
+            alert.error("Exceed stock Limit");
+        }
+    }
+
+    const decreaseQty = () => {
+        if(quantity > 1){
+            const newQuantity = quantity - 1;
+            setQuantity(newQuantity);
+            dispatch(updateCartQuantity(fooditem._id, newQuantity, alert));
+        }
+        else{
+            setQuantity(0);
+            setShowButtons(false);
+            dispatch(removeItemFromCart(fooditem._id));
+        }
+    };
+
+    const addToCartHandler = () => {
+        if(!isAuthenticated && !user){
+            return navigate("/users/login");
+        }
+        if(fooditem && fooditem._id){
+            dispatch(addItemToCart(fooditem._id, restaurant, quantity, alert));
+            setShowButtons(true);
+        }
+        else{
+            console.log("Food Item id not defines");
+        }
+    };
 
   return (
     <div className="col-sm-12 col-md-6 col-lg-3 my-3">
@@ -29,10 +92,32 @@ export default function FoodItem({fooditem}) {
                     {fooditem.price}
                     <br />
                 </p>
-                <button type='button' id='cart_btn'
-                    className="btn btn-primary d-inline ml-4">
-                    Add to Cart
-                </button>
+                
+                {
+                    !showButtons ? (
+                        <button type='button' id="cart_btn" className='btn btn-primary d-inline ml-4' 
+                            disabled={fooditem.stock === 0}
+                            onClick={addToCartHandler}
+                        >
+                            Add to Cart
+                        </button>
+                    ) : (
+                        <div className="stockCounter d-inline">
+                            <span className="btn btn-danger minus" onClick={decreaseQty}>
+                                -
+                            </span>
+                            <input 
+                                type='number'
+                                className='form-control count d-inline'
+                                value={quantity}
+                                readOnly
+                            />
+                            <span className="btn btn-primary plus" onClick={increaseQty}>
+                                +
+                            </span>
+                        </div>
+                    )
+                }
                 <br />
                 <p>
                     Status : {" "}
